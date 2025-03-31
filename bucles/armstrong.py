51ded9de9c7fac9084325b9fe88afd34fa8619dd
Daniel <90976801+Daluca21@users.noreply.github.com>
2025-03-31 07:35:26 -0500

    fix

--- bucles/armstrong.py
+++ bucles/armstrong.py
@@ -1,7 +1,7 @@
 
-for i in range(1, 10):
-    for j in range(1, 10):
-        for k in range(1, 10):
+for i in range(0, 10):
+    for j in range(0, 10):
+        for k in range(0, 10):
             ci = i*i*i
             cj = j*j*j
             ck = k*k*k

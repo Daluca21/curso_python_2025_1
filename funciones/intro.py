65330d7b303e9a7827cfed8ef2004f15953c364b
Daniel <90976801+Daluca21@users.noreply.github.com>
2025-05-12 06:18:32 -0500

    Agregado manejo de variables globales y locales

--- funciones/intro.py
+++ funciones/intro.py
@@ -1,8 +1,8 @@
-import random
 
 def saludar():
     print("Hola")
-    print("Como estas?")
+
+import random
 
 def rotar(lista):
     lista_copia = lista.copy()
@@ -21,6 +21,31 @@ def generar_lista_aleatoria(n = 10, l = 1, r = 5):
     for i in range(n):
         lista.append(random.randint(l, r))
     return lista
+
+
+n = 10
+l = 0
+r = 10
+#generar una lista de numeros aleatorios
+
+lista = []
+for i in range(n):
+    lista.append(random.randint(l, r))
+lista2 = []
+for i in range(n):
+    lista2.append(random.randint(l, r))
+lista3 = []
+for i in range(n):
+    lista3.append(random.randint(l, r))
+lista4 = []
+for i in range(n):
+    lista4.append(random.randint(l, r))
+lista5 = []
+for i in range(n):
+    lista5.append(random.randint(l, r))
+lista6 = []
+for i in range(n):
+    lista6.append(random.randint(l, r))
 
 
 def sumar(a = 0, b = 0):
@@ -43,11 +68,12 @@ def promediar_sin_extremos(lista):
     promedio = (suma - (mx + mn))/(len(lista) - 2)
     return promedio
 
-generar_lista_aleatoria(12, 4)
-generar_lista_aleatoria(19, 1, 20)
-generar_lista_aleatoria(15)
-generar_lista_aleatoria()
-generar_lista_aleatoria(r = 14, n = 9)
+generar_lista_aleatoria(12, 4) #indico la n, indica la l
+generar_lista_aleatoria(19, 1, 20)# indico la n, indico la l, indico la r
+generar_lista_aleatoria(15) #indico la n
+generar_lista_aleatoria() 
+generar_lista_aleatoria(r = 14, n = 9) #indico la n , indico la r
+generar_lista_aleatoria(r = 10, l = -2, n = 131)
 
 print(sumar())
 print(sumar(5, 4))
@@ -62,3 +88,22 @@ for i in range(5):
 
 print(aproximar_euler(3))
 print(promediar_sin_extremos(lista_aleatoria))
+
+
+i = -10
+
+def calcular():
+    global i
+    i = 0
+    while(i < 10):
+        print(i)
+        i += 1
+
+def calcular2():
+    global i
+    while(i < 10):
+        print(i)
+        i += 1
+
+calcular()
+calcular2()
